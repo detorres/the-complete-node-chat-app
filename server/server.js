a33d53198433f3cf965d937b813782cd265b47ef
Andy de Torres <detorres@stevens>
2017-09-01 12:31:21 -0700

    greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -16,6 +16,18 @@ io.on('connection', (socket) =>
 {
   console.log('New user connected.');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat.',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined.',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) =>
   {
     console.log('Message created', message);
@@ -24,6 +36,11 @@ io.on('connection', (socket) =>
       text: message.text,
       createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 
   socket.on('disconnect', () =>
